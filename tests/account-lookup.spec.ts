46ee1ff1cbf48f5e51b2d4e64e436f16160be138
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-06-23 11:34:22 +0300

    More tests.

--- tests/account-lookup.spec.ts
+++ tests/account-lookup.spec.ts
@@ -1,21 +1,24 @@
-import { AccountAddress, AssetCode, Currency, ExpenseSink, IncomeSource, PluginCode } from '@dataplug/tasenor-common'
+import { AccountAddress, AccountType, AssetCode, Currency, ExpenseSink, IncomeSource, PluginCode, warning } from '@dataplug/tasenor-common'
 
 export interface AccountLookupCondition {
   tax: AssetCode | ExpenseSink | IncomeSource
   currency?: Currency
   plugin?: PluginCode
+  type?: AccountType
 }
 
 export interface AccountLookupOption {
   defaultCurrency: Currency,
-  plugin: PluginCode
+  plugin: PluginCode,
+  strict?: boolean
 }
 
-export function conditions(addr: AccountAddress): AccountLookupCondition | null {
+export function conditions(addr: AccountAddress, strict: boolean): AccountLookupCondition | null {
   const [reason, type, asset] = addr.split('.')
 
   if (reason === 'debt') {
     if (type === 'currency') {
+      // TODO: Or more general creditor instead?
       return { tax: 'OTHER_CREDITORS', currency: asset as Currency }
     }
   }
@@ -28,31 +31,70 @@ export function conditions(addr: AccountAddress): AccountLookupCondition | null 
       return null
     }
   }
-  throw new Error(`No SQL conversion known for account address ${addr}.`)
+
+  if (reason === 'distribution') {
+    return null
+  }
+
+  if (reason === 'dividend') {
+    if (type === 'currency') {
+      // TODO: How to handle different sub-types of dividend?
+      return { tax: 'DIVIDEND', currency: asset as Currency }
+    }
+  }
+
+  if (reason === 'expense') {
+    if (type === 'currency') {
+      // TODO: Is there any change that we could handle this?
+      return null
+    }
+    if (type === 'statement') {
+      return { type: AccountType.EXPENSE, tax: asset as ExpenseSink }
+    }
+  }
+
+  const message = `No SQL conversion known for account address '${addr}'.`
+  if (strict) {
+    throw new Error(message)
+  }
+  warning(message)
+  return null
 }
 
 function address2sql(addr: AccountAddress, options: AccountLookupOption): string | null {
-  const cond = conditions(addr)
+  const cond = conditions(addr, !!options.strict)
   if (cond === null) {
     return null
   }
+
+  const addSql: string[] = []
   if (cond.currency === options.defaultCurrency) {
+    addSql.push(`(data->>'currency' = '${cond.currency}' OR data->>'currency' IS NULL)`)
     delete cond.currency
   }
+
+  if (cond.type) {
+    addSql.push(`(type = '${cond.type}')`)
+    delete cond.type
+  }
+
   cond.plugin = options.plugin
+
   const sql = Object.keys(cond).map(key => `(data->>'${key}' = '${cond[key]}')`)
-  return sql.join(' AND ')
+
+  return [...sql, ...addSql].join(' AND ')
 }
 
 test('Convert account address to account default', async () => {
   const addr2sql = (addr: string, options: Record<string, string>) => address2sql(addr as AccountAddress, {
     defaultCurrency: 'EUR',
     plugin: 'SomeImport' as PluginCode,
+    strict: false,
     ...options
   })
 
   expect(addr2sql('debt.currency.EUR', {})).toBe(
-    "(data->>'tax' = 'OTHER_CREDITORS') AND (data->>'plugin' = 'SomeImport')"
+    "(data->>'tax' = 'OTHER_CREDITORS') AND (data->>'plugin' = 'SomeImport') AND (data->>'currency' = 'EUR' OR data->>'currency' IS NULL)"
   )
   expect(addr2sql('debt.currency.SEK', {})).toBe(
     "(data->>'tax' = 'OTHER_CREDITORS') AND (data->>'currency' = 'SEK') AND (data->>'plugin' = 'SomeImport')"
@@ -61,81 +103,78 @@ test('Convert account address to account default', async () => {
     "(data->>'tax' = 'OTHER_CREDITORS') AND (data->>'currency' = 'USD') AND (data->>'plugin' = 'SomeImport')"
   )
   expect(addr2sql('deposit.currency.EUR', {})).toBe(
-    "(data->>'tax' = 'CASH') AND (data->>'plugin' = 'SomeImport')"
+    "(data->>'tax' = 'CASH') AND (data->>'plugin' = 'SomeImport') AND (data->>'currency' = 'EUR' OR data->>'currency' IS NULL)"
   )
   expect(addr2sql('deposit.external.EUR', {})).toBe(
     null
   )
-  /*
   expect(addr2sql('distribution.currency.EUR', {})).toBe(
-    ''
+    null
   )
   expect(addr2sql('distribution.statement.LISTED_DIVIDEND', {})).toBe(
-    ''
+    null
   )
   expect(addr2sql('dividend.currency.EUR', {})).toBe(
-    ''
+    "(data->>'tax' = 'DIVIDEND') AND (data->>'plugin' = 'SomeImport') AND (data->>'currency' = 'EUR' OR data->>'currency' IS NULL)"
   )
   expect(addr2sql('dividend.currency.SEK', {})).toBe(
-    ''
+    "(data->>'tax' = 'DIVIDEND') AND (data->>'currency' = 'SEK') AND (data->>'plugin' = 'SomeImport')"
   )
   expect(addr2sql('dividend.currency.USD', {})).toBe(
-    ''
+    "(data->>'tax' = 'DIVIDEND') AND (data->>'currency' = 'USD') AND (data->>'plugin' = 'SomeImport')"
   )
   expect(addr2sql('expense.currency.EUR', {})).toBe(
-    ''
+    null
   )
   expect(addr2sql('expense.statement.ADMIN', {})).toBe(
-    ''
+    "(data->>'tax' = 'ADMIN') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.BANKING_FEE', {})).toBe(
-    ''
+    "(data->>'tax' = 'BANKING_FEE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.EQUIPMENT', {})).toBe(
-    ''
+    "(data->>'tax' = 'EQUIPMENT') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.FURNITURE', {})).toBe(
-    ''
+    "(data->>'tax' = 'FURNITURE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.HARDWARE', {})).toBe(
-    ''
+    "(data->>'tax' = 'HARDWARE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.INFORMATION', {})).toBe(
-    ''
+    "(data->>'tax' = 'INFORMATION') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.INTEREST_EXPENSE', {})).toBe(
-    ''
+    "(data->>'tax' = 'INTEREST_EXPENSE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.INTERNET', {})).toBe(
-    ''
+    "(data->>'tax' = 'INTERNET') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.MEETINGS', {})).toBe(
-    ''
+    "(data->>'tax' = 'MEETINGS') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.NEEDS_MANUAL_INSPECTION', {})).toBe(
-    ''
+    "(data->>'tax' = 'NEEDS_MANUAL_INSPECTION') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.PHONE', {})).toBe(
-    ''
+    "(data->>'tax' = 'PHONE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.POSTAGE', {})).toBe(
-    ''
+    "(data->>'tax' = 'POSTAGE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.SOFTWARE', {})).toBe(
-    ''
+    "(data->>'tax' = 'SOFTWARE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.STOCK_BROKER_SERVICE_FEE', {})).toBe(
-    ''
-  )
-  expect(addr2sql('expense.statement.STOCK_BROKER_SERVICE_FEE', {})).toBe(
-    ''
+    "(data->>'tax' = 'STOCK_BROKER_SERVICE_FEE') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.TICKET', {})).toBe(
-    ''
+    "(data->>'tax' = 'TICKET') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
   expect(addr2sql('expense.statement.TRADE_LOSS_STOCK', {})).toBe(
-    ''
+    "(data->>'tax' = 'TRADE_LOSS_STOCK') AND (data->>'plugin' = 'SomeImport') AND (type = 'EXPENSE')"
   )
+  /*
   expect(addr2sql('fee.currency.EUR', {})).toBe(
     ''
   )
@@ -152,9 +191,6 @@ test('Convert account address to account default', async () => {
     ''
   )
   expect(addr2sql('income.statement.FINLAND_SALES', {})).toBe(
-    ''
-  )
-  expect(addr2sql('income.statement.FINLAND_SALES2', {})).toBe(
     ''
   )
   expect(addr2sql('income.statement.LISTED_DIVIDEND', {})).toBe(
