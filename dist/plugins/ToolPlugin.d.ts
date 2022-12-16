07e583a032572f40d66ac011538c042a5afca0b7
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-12-16 18:33:41 +0200

    Support for REST api in tool plugins.

--- dist/plugins/ToolPlugin.d.ts
+++ dist/plugins/ToolPlugin.d.ts
@@ -3,4 +3,9 @@ import { BackendPlugin } from './BackendPlugin';
  * A generic tool.
  */
 export declare class ToolPlugin extends BackendPlugin {
+    GET(query: any): Promise<unknown>;
+    DELETE(query: any): Promise<unknown>;
+    POST(data: any): Promise<unknown>;
+    PUT(data: any): Promise<unknown>;
+    PATCH(data: any): Promise<unknown>;
 }
--- src/plugins/ToolPlugin.ts
+++ src/plugins/ToolPlugin.ts
@@ -5,4 +5,38 @@ import { BackendPlugin } from './BackendPlugin'
  */
 export class ToolPlugin extends BackendPlugin {
 
+  /**
+   * Handler for GET request.
+   */
+  async GET(query): Promise<unknown> {
+    return undefined
+  }
+
+  /**
+   * Handler for DELETE request.
+   */
+  async DELETE(query): Promise<unknown> {
+    return undefined
+  }
+
+  /**
+   * Handler for POST request.
+   */
+  async POST(data): Promise<unknown> {
+    return undefined
+  }
+
+  /**
+   * Handler for PUT request.
+   */
+  async PUT(data): Promise<unknown> {
+    return undefined
+  }
+
+  /**
+   * Handler for PATCH request.
+   */
+  async PATCH(data): Promise<unknown> {
+    return undefined
+  }
 }
