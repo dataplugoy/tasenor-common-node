1e93ad24dc021b5430366d3156d514aac2e55bfc
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2023-03-21 20:15:34 +0200

    Handle single match.

--- dist/import/TransactionRules.d.ts
+++ dist/import/TransactionRules.d.ts
@@ -79,6 +79,9 @@ import { TransactionImportHandler } from './TransactionImportHandler';
  * {
  *    "name": "Name of the rule",
  *    "filter": "<expression>",
+ *    "options": {
+ *      <optional flags>
+ *    },
  *    "result": [
  *       <transfer1>, <transfer2>...
  *    ]
@@ -96,6 +99,11 @@ import { TransactionImportHandler } from './TransactionImportHandler';
  * The structure of transfers are explained in {@link TransferAnalyzer}.
  *
  * The syntax of the filter and result is explained in {@link RulesEngine}.
+ *
+ * Currently one one boolean option is supported: `singleMatch` which means that matching any of the lines in the
+ * segment suffices and the parsing result is returned immediately when matching rule is found. The rest of the
+ * lines are ignored. It is useful when for example using `sum(lines, 'field')` to gather values from all lines
+ * of the segment at once.
  *
  * ### Questions
  *
@@ -201,6 +209,7 @@ export declare class TransactionRules {
      * In addition the following special variables are provided:
      * * `config` - all configuration variables
      * * `rule` - the current rule we are evaluating
+     * * `options` - the options of the current rule we are evaluating
      * * `text` - original text of the corresponding line
      * * `lineNumber` - original line number of the corresponding line
      * If the filter match is found, then questions are provided to UI unless already
